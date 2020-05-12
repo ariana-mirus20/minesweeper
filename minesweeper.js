dcd48f4478dd01d5b7f01824b2b91d8e05af523a
Ariana Mirus <arianamirus@gmail.com>
2020-05-13 11:52:24 +1200

    board 4x4 added to game

--- minesweeper.js
+++ minesweeper.js
@@ -1,12 +1,41 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-// var board = 
+var board = {
+
+  cells: [{
+    row: 0,
+    col: 0,
+    isMine: false,
+    hidden: true,
+  },
+  {
+    row: 0,
+    col: 1,
+    isMine: false,
+    hidden: true,
+  },
+  {
+    row: 1,
+    col: 0,
+    isMine: false,
+    hidden: true,
+  },
+  {
+    row: 1,
+    col: 1,
+    isMine: false,
+    hidden: true,
+  }], 
+}
 
 function startGame () {
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
+  //for (var i = 0; i < board.cells.length; i++)
 }
+//var cell = board.cells[i]
+//cell.surroundingMines = countSurroundingMines(cell)
 
 // Define this function to look for a win condition:
 //
